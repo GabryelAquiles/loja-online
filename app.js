d612afb8ba0f2f00a1008a4f72bd5348fac71f21
GabryelAquiles <gabryel.tlopes@gmail.com>
2026-02-04 17:31:17 -0300

    Update Firebase SDK version and configuration

--- app.js
+++ app.js
@@ -96,3 +96,28 @@ async function inicializarSaaS() {
 // Inicia o sistema
 inicializarSaaS();
 
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyC6g9nuso280y5ezxSQyyuF5EljE9raz0M",
+    authDomain: "aquiles-sw-saas.firebaseapp.com",
+    projectId: "aquiles-sw-saas",
+    storageBucket: "aquiles-sw-saas.firebasestorage.app",
+    messagingSenderId: "878262536684",
+    appId: "1:878262536684:web:e32ac0b9755ca101e398c9",
+    measurementId: "G-K4BW142MY2"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
+
+
